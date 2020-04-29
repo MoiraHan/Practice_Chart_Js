b581afe07b13eea0ca2ce724dc95a6de8add4539
moira_han <moira_han@mail.csf.org.tw>
2020-04-29 17:57:05 +0800

    * 加入 Practice Controller * 調整首頁、起始頁 * 加入 Chart Js 的套件 ( Js & Css )

--- Practice_Chart_Js/App_Start/RouteConfig.cs
+++ Practice_Chart_Js/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Practice_Chart_Js
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Practice", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
